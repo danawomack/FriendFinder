b55642e81544b8ab4401fef9c4242b2b1c970e18
Dana Womack <Dana@DanaWomacksMBP.fios-router.home>
2018-05-08 17:11:27 -0400

    updating all files

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friendsArray = [
+	{
+	  "name":"Ciara",
+	  "photo":"https://www.etonline.com/sites/default/files/styles/max_970x546/public/images/2018-01/ciara_gettyimages_883381254.jpg?itok=64mOcle5&h=c673cd1c",
+	  "sex":"female",
+	  "scores":[
+	     5,
+	     1,
+	     4,
+	     4,
+	     5,
+	     1,
+	     2,
+	     5,
+	     4,
+	     1
+	   ]
+	},]
